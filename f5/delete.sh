6c64927a689b023180ac86b2a9462f0cb7bc1cf5
yuchunyun <yuchunyun@zdns.cn>
2019-01-28 15:42:20 +0800

    f5

--- f5/delete.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-kubectl delete -f pod-svc.yaml 
-kubectl delete -f ingress-conf.yaml 
-kubectl delete -f ingress-svc.yaml 
-kubectl delete -f f5-conf.yaml 
-kubectl delete -f f5.yaml 
-kubectl delete -f ingress.yaml 
--- f5/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-kubectl apply -f f5.yaml 
-kubectl apply -f ingress.yaml 
-kubectl apply -f ingress-svc.yaml 
-kubectl apply -f f5-conf.yaml 
-kubectl apply -f pod-svc.yaml 
-kubectl apply -f ingress-conf.yaml 
